b79c99f209283663be9054828646c8384187f38d
Liam Conway <11491642+Mavestri@users.noreply.github.com>
2019-04-09 12:32:36 -0600

    Existing Content Push
    Contains working login system, working dashboard, working embedded page with demo content (home, author submit)

---
+++ res/js/dash.js
@@ -0,0 +1,27 @@
+let submenus = {
+    authorMenu: document.getElementById("author-stories"),
+    reviewerMenu: document.getElementById("reviewer-stories"),
+    editorMenu: document.getElementById("editor-stories")
+}
+
+let embedFrame = document.getElementById("contentEmbed");
+let path = document.getElementById("path");
+
+function openSubmenu(menuId) {
+    for(var menuName in submenus) {
+        if(menuId === menuName && submenus[menuName].style.display != 'block')
+            submenus[menuName].style.display = 'block';
+        else if(submenus[menuName].style.display === 'block')
+            submenus[menuName].style.display = 'none';
+    }
+}
+
+function openPage(url) {
+    embedFrame.src = url;
+    path.innerText = url.replace(/\//g, ' > ').replace(/_/g, ' ').replace(/\b[a-z]/g, function(l) { return l.toUpperCase(); }).replace(/.html/gi, '');
+    return true;
+}
+
+function logout() {
+	
+}
